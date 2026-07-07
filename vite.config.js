0479a76e3455d9c213fc8550ccd4116cb46cb1e3
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2026-07-08 03:48:48 +0600

    Configure npm package for JS and TS consumers

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
 import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.js"),
@@ -10,7 +15,7 @@ export default defineConfig({
       formats: ["es"]
     },
     rollupOptions: {
-      external: ["react"],
+      external: ["react", "react-dom"],
       output: {
         assetFileNames: "assets/[name][extname]"
       }
